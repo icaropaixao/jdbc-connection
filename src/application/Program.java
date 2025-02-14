dd170d09bfe9b4e3c3146767382183fe193ac81d
Ícaro Reis <icaroreis1982@gmail.com>
2025-02-14 20:26:29 -0300

    Update Program.java

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,6 +7,7 @@ import java.sql.Connection;
 public class Program {
     public static void main(String[] args) {
 
+        // exemple
         Connection con = DB.getConnection();
         DB.closeConnection();
     }
